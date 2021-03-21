89b22539e035826aad9d4a8b46b70f16415cdecd
aarav010710 <76567151+aarav010710@users.noreply.github.com>
2021-03-21 16:13:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var changetext =["mom","me"]
+
+var changeImage =["https://i.pinimg.com/736x/f8/a9/dd/f8a9dd5024d6ebed33ae33db724d84e3.jpg","https://thumbs.dreamstime.com/b/happy-cute-kid-boy-ready-to-go-school-166126017.jpg"]
+
+
+        function changepicture() {
+            var image = document.getElementById("changeImage");
+        }
+            function changename() {
+            var image = document.getElementById("changetext");
+            }
